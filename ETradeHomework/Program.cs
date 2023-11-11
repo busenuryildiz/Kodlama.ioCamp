f8ffd2fa81a38276f46f13fbc05e72aa6c039efb
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-11 20:42:06 +0300

    Day 1-2

---
+++ ETradeHomework/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ETradeHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product();
+            product1.ProductName = "Laptop";
+            product1.Description = "256 gb SSD";
+            product1.UnitPrice = 25000;
+            
+            Product product2 = new Product();
+            product2.ProductName = "Mouse";
+            product2.Description = "Bluetooth";
+            product2.UnitPrice = 500;
+
+
+
+            Product[] products = new Product[] { product1, product2 };
+
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ProductName + " : " + product.Description + " : " + product.UnitPrice);
+            }
+        }
+    }
+    class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int UnitPrice { get; set; }
+    }
+
+}
