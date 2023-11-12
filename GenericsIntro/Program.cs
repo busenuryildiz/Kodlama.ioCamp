54840c41069cdfc444e35f0c03b8cc79b491266c
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-12 15:38:53 +0300

    Day 4

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            //List<string> liste = new List<string>();
+            //Console.WriteLine(liste.Count);
+
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+        }
+    }
+}
