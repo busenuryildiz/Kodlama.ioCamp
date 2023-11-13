e4a7c45337c14270c696eb6b9450409c2197c3e5
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-13 14:26:24 +0300

    Day 5

---
+++ OOP3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService= new DatabaseLoggerService();
+            ILoggerService fileLoggerService= new FileLoggerService();
+
+            List<ILoggerService> loggers=new List<ILoggerService> {new SmsLoggerService(),new FileLoggerService()};
+
+            BasvuruManager basvuruManager= new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), loggers);
+
+            List<IKrediManager> krediler=new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
